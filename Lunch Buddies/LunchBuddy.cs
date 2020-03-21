d1488316be43498441601c0bee1fd276a82ea575
Alesha Reed <aleshak83@gmail.com>
2020-03-21 13:43:13 -0500

    updated eat

--- Lunch Buddies/LunchBuddy.cs
+++ Lunch Buddies/LunchBuddy.cs
@@ -11,20 +11,38 @@ namespace Lunch_Buddies
 
         public LunchBuddy(string firstName, string lastName)  //constructor
         {
-            FirstName = "Alice";
-            LastName = "Jenkins";
+            FirstName = firstName;
+            LastName = lastName;
 
         }
         public string Eat()
         {
             var restaurant = new Restaurant();
-            Console.WriteLine($"{FirstName} + {LastName} is at {restaurant.Name}");
+            Console.WriteLine($"{FirstName} {LastName} is currenty at the world famous {restaurant.Name}");
             return restaurant.Name;
         }
 
         public void Eat(string food)
         {
-            Console.WriteLine($"{FirstName} +{LastName} ate {food} today.");
+            Console.WriteLine($"{FirstName} {LastName} ate {food} today.");
+        }
+
+        public void Eat(List<LunchBuddy> companions)
+        {
+            var restaurant = new Restaurant();
+            foreach (var companion in companions)
+            {
+                Console.WriteLine($"At {restaurant.Name}, {companion.FirstName} {companion.LastName} ate lunch");
+            }
+        }
+
+        public void Eat(List<LunchBuddy> companions,string food)
+        {
+            var restaurant = new Restaurant();
+            foreach (var companion in companions)
+            {
+                Console.WriteLine($"While at {restaurant.Name},{FirstName} and {companion.FirstName} where partying there butts off and eating {food}");
+            }
         }
     }
 }
--- Lunch Buddies/Program.cs
+++ Lunch Buddies/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lunch_Buddies
 {
@@ -8,7 +9,18 @@ namespace Lunch_Buddies
         {
             Console.WriteLine("Lunch Program");
 
-            Eat();
+            List<LunchBuddy> companions = new List<LunchBuddy> { };
+            companions.Add(new LunchBuddy("Nathan", "Pride"));
+            companions.Add(new LunchBuddy("Minor", "Reed"));
+
+            var lunchBuddy = new LunchBuddy("I Do Not", "know");
+
+            lunchBuddy.Eat();
+            lunchBuddy.Eat("cake");
+            lunchBuddy.Eat(companions);
+            lunchBuddy.Eat(companions, "cake");
+
+
 
         }
     }
